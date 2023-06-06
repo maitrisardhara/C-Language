b88179068ec2e849d7b20e9d0f8d1d468a565e3f
maitrisardhara <134732532+maitrisardhara@users.noreply.github.com>
2023-06-06 16:39:06 +0530

    Add files via upload

---
+++ day10/months name.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	char a;
+	 printf("Enter value:");
+	 scanf("%c",&a);
+	 
+	 switch(a){
+	 	
+	 	case 'j':
+	 		printf("January");
+	 		break;
+	 
+	 	case 'f':
+	 		printf("February");
+	 		break;
+		
+		case 'm':
+	 		printf("March");
+	 		break;
+			 
+		case 'a':
+	 		printf("April");
+	 		break;	 	 	
+	 		
+	 	case 'M':
+	 		printf("May");
+	 		break;
+			 
+	    case 'J':
+	 		printf("June");
+	 		break;	
+	 		
+	 	case 'u':
+	 		printf("July");
+	 		break;	
+       
+       case 'A':
+	 		printf("August");
+	 		break;
+	 		
+	 	case 's':
+	 		printf("September");
+	 		break;
+	 		
+	 	case 'o':
+	 		printf("October");
+	 		break;
+			 
+		case 'n':
+	 		printf("November");
+	 		break;
+			 
+		case 'd':
+	 		printf("December");
+	 		break;
+	 		
+	 	
+			 	  	
+	 }
+	 
+return 0;
+
+}
