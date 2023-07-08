d87b20f909e38ef41945e7abe54232020cd0ae28
maitrisardhara <134732532+maitrisardhara@users.noreply.github.com>
2023-07-08 11:53:09 +0530

    Add files via upload

---
+++ Indicator/q4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+void len(char *a){
+	
+	int length = 0;
+	
+	while(*a != '\0'){
+		
+		length++;
+		a++;
+	}
+	
+}
+
+int main(){
+	
+	char a[100];
+	
+	printf("Enter string : ");
+	scanf("%s",a);
+	
+    int length = strlen(a);
+	
+	printf("Length of string : %d\n",length);
+	
+	return 0;
+}
